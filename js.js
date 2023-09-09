60ef48952838935d65c70b34bf86ff4de1949e98
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2023-09-09 14:55:23 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,103 @@
+var hours,minutes,seconds;
+
+function timer()
+{
+    if(localStorage.getItem("curentHour") != null)
+    {
+        load();
+    }
+    else
+    {
+        hours = Math.floor(Math.random()*3 +1);
+        minutes =  Math.floor(Math.random()*60);
+        seconds =  Math.floor(Math.random()*60);
+        localStorage.setItem("curentHour", hours);
+        localStorage.setItem("curentMinutes", minutes);
+        localStorage.setItem("curentSeconds", seconds);
+        if(hours < 10)
+        {
+            hours = "0" + hours;
+        }
+        if(minutes < 10)
+        {
+            minutes = "0" + minutes;
+        }
+        if(seconds < 10)
+        {
+            seconds = "0" + seconds;
+        }
+        document.getElementById("hour").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+    }
+}
+setInterval(timerCountDown, 1000);
+function save()
+{
+    localStorage.setItem("curentHour", hours);
+    localStorage.setItem("curentMinutes", minutes);
+    localStorage.setItem("curentSeconds", seconds);
+}
+function load()
+{
+
+    hours =parseFloat(localStorage.getItem("curentHour"));
+    minutes = parseFloat(localStorage.getItem("curentMinutes"));
+    seconds = parseFloat(localStorage.getItem("curentSeconds"));
+   if(hours < 10)
+   {
+       hours = "0" + hours;
+   }
+   if(minutes < 10)
+   {
+       minutes = "0" + minutes;
+   }
+   if(seconds < 10)
+   {
+       seconds = "0" + seconds;
+   }
+   document.getElementById("hour").innerText = hours;
+   document.getElementById("minutes").innerText =  minutes;
+   document.getElementById("seconds").innerText = seconds;
+}
+function timerCountDown()
+{
+    var currentTimeRemaining =  document.getElementById("hour").innerText +  document.getElementById("minutes").innerText +  document.getElementById("seconds").innerText;
+    if( currentTimeRemaining != 0)
+    {
+        if(document.getElementById("seconds").innerText > 0)
+        {
+            document.getElementById("seconds").innerText -= 1;
+            if(document.getElementById("seconds").innerText < 10)
+    
+            {
+                document.getElementById("seconds").innerText = "0" + document.getElementById("seconds").innerText;
+            }
+           
+        }
+        if(document.getElementById("seconds").innerText <= 0)
+        {
+            if(document.getElementById("minutes").innerText > 0)
+            {
+                document.getElementById("minutes").innerText -= 1;
+                if( document.getElementById("minutes").innerText < 10)
+                {
+                    document.getElementById("minutes").innerText = "0" +  document.getElementById("minutes").innerText;
+                }
+                document.getElementById("seconds").innerText = 60;
+            }
+        }
+        if(document.getElementById("seconds").innerText <= 0 && document.getElementById("minutes").innerText <= 0 && document.getElementById("hour").innerText >0)
+        {
+            document.getElementById("hour").innerText -= 1;
+            document.getElementById("hour").innerText = "0" + document.getElementById("hour").innerText;
+            document.getElementById("minutes").innerText = 60;
+        }
+        hours = document.getElementById("hour").innerText;
+        minutes =  document.getElementById("minutes").innerText;
+        seconds =  document.getElementById("seconds").innerText;
+        save();
+    }
+    
+   
+}
